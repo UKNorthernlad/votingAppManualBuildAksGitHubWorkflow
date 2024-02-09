b52b6293c420fb29b0ea3a66624f53fba9e76d58
Billy Bunter <billy@bunter.com>
2024-02-09 16:21:30 +0000

    Calico setup

--- build ACR AKS Cluster.ps1
+++ build ACR AKS Cluster.ps1
@@ -1,7 +1,6 @@
 ﻿# Taken from https://learn.microsoft.com/en-us/azure/aks/kubernetes-action
 # Authenticate with Azure Container Registry (ACR) from Azure Kubernetes Service (AKS)
 
-
 $resourceGroup = "containers"
 $acr="eboracr"
 $aksClusterName = "eborcluster99"
@@ -9,112 +8,28 @@ $subscriptionId = "XXXXXX"
 
 az group create -n $resourceGroup -l "westeurope"
 az acr create -n $acr -g $resourceGroup --sku basic
-az aks create -n $aksClusterName -g $resourceGroup --generate-ssh-keys --attach-acr $acr
+az aks create -n $aksClusterName -g $resourceGroup --generate-ssh-keys --attach-acr $acr  --network-plugin azure
 az aks get-credentials -g $resourceGroup -n $aksClusterName
 
 # Import the Cat & Dog Voting application
 az acr import  -n $acr --source mcr.microsoft.com/oss/bitnami/redis:6.0.8 --image azure-vote-back:v1
 az acr import  -n $acr --source mcr.microsoft.com/azuredocs/azure-vote-front:v1 --image azure-vote-front:v1
 
-# Create the k8s application manifest
-'
-apiVersion: apps/v1
-kind: Deployment
-metadata:
-  name: azure-vote-back
-spec:
-  replicas: 1
-  selector:
-    matchLabels:
-      app: azure-vote-back
-  template:
-    metadata:
-      labels:
-        app: azure-vote-back
-    spec:
-      nodeSelector:
-        "kubernetes.io/os": linux
-      containers:
-      - name: azure-vote-back
-        image: eboracr.azurecr.io/azure-vote-back:v1
-        env:
-        - name: ALLOW_EMPTY_PASSWORD
-          value: "yes"
-        resources:
-          requests:
-            cpu: 100m
-            memory: 128Mi
-          limits:
-            cpu: 250m
-            memory: 256Mi
-        ports:
-        - containerPort: 6379
-          name: redis
----
-apiVersion: v1
-kind: Service
-metadata:
-  name: azure-vote-back
-spec:
-  ports:
-  - port: 6379
-  selector:
-    app: azure-vote-back
----
-apiVersion: apps/v1
-kind: Deployment
-metadata:
-  name: azure-vote-front
-spec:
-  replicas: 1
-  selector:
-    matchLabels:
-      app: azure-vote-front
-  template:
-    metadata:
-      labels:
-        app: azure-vote-front
-    spec:
-      nodeSelector:
-        "kubernetes.io/os": linux
-      containers:
-      - name: azure-vote-front
-        image: eboracr.azurecr.io/azure-vote-front:v1
-        resources:
-          requests:
-            cpu: 100m
-            memory: 128Mi
-          limits:
-            cpu: 250m
-            memory: 256Mi
-        ports:
-        - containerPort: 80
-        env:
-        - name: REDIS
-          value: "azure-vote-back"
----
-apiVersion: v1
-kind: Service
-metadata:
-  name: azure-vote-front
-spec:
-  type: LoadBalancer
-  ports:
-  - port: 80
-  selector:
-    app: azure-vote-front
-' >> application.yaml
+# Import the k8s application manifest
+kubectl apply -f .\application.yaml
+#kubectl get service/azure-vote-front --watch
 
+# Install Calico Operator
+# https://docs.tigera.io/calico/latest/getting-started/kubernetes/managed-public-cloud/aks
+kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.27.0/manifests/tigera-operator.yaml
 
-kubectl apply -f application.yaml
-
-kubectl get service/azure-vote-front --watch
+# Configure the Calico installation.
+kubectl create -f .\calicoInstallation.yaml
 
 
 ###################################################
 # Setup AAD Service Principal for GH authentication
 ###################################################
-
 
 # https://learn.microsoft.com/en-us/azure/developer/github/connect-from-azure?tabs=azure-cli%2Clinux
 # Use GitHub Actions to connect to Azure
@@ -124,7 +39,6 @@ $appId = az ad app create --display-name GitHubActionsApp  --query "appId" --out
 $spId = az ad sp create --id $appId --query "id" --output tsv 
 
 az role assignment create --role contributor --subscription $subscription --assignee-object-id  $spId --assignee-principal-type ServicePrincipal --scope /subscriptions/$subscriptionId/resourceGroups/$resourceGroup
-
 
 # Now create the following Secret in a new environment called "Testing"
 
@@ -136,6 +50,12 @@ AZURE_CREDENTIALS
     "clientId":       "XXXX",
 }
 
+
+
+
+
+# Network policies replacment??
+# https://learn.microsoft.com/en-us/azure/aks/use-pod-security-policies
 
 
 
